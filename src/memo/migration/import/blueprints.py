70c9d8410368e0bbcf4f3748f74a23f396f023e5
muellers-saw-leipzig <muellers@saw-leipzig.de>
2021-08-10 16:28:35 +0200

    some more blueprints adapted to import memo data

--- src/memo/migration/import/blueprints.py
+++ src/memo/migration/import/blueprints.py
@@ -17,15 +17,20 @@ from zope.annotation import IAnnotations
 from z3c.relationfield import RelationValue
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
-
+from collective.relationhelpers import api as relapi
 from plone.app.uuid.utils import uuidToObject
 
+#from collective.transmogrifier.utils import Expression
+from collective.transmogrifier.utils import defaultMatcher
+from datetime import datetime
+from collective.transmogrifier.utils import traverse
+#from collective.transmogrifier.utils import resolvePackageReferenceOrFile
 import transaction
+from zope.schema import URI
 import logging
 
-
-logger = logging.getLogger('Transmogrifier')
-
+MYKEY = 'saw.memo.migration.blueprints'
+logger = logging.getLogger(MYKEY)
 
 @implementer(ISection)
 @provider(ISectionBlueprint)
@@ -40,28 +45,10 @@ class Example(object):
         self.previous = previous
         self.context = transmogrifier.context
 
-        if 'path-key' in options:
-            pathkeys = options['path-key'].splitlines()
-        else:
-            pathkeys = defaultKeys(options['blueprint'], name, 'path')
-        self.pathkey = Matcher(*pathkeys)
 
     def __iter__(self):
         for item in self.previous:
-            pathkey = self.pathkey(*item.keys())[0]
-
-            # if you need to get the object (after the constructor part)
-            obj = self.context.unrestrictedTraverse(
-                safe_unicode(item['_path'].lstrip('/')).encode('utf-8'),
-                None,
-            )
-            if not obj:
-                yield item
-                continue
-
-            # do things here
-
-            logger.info('[processing path] %s', pathkey)
+           
 
             # always end with yielding the item,
             # unless you don't want it imported, or want
@@ -86,21 +73,71 @@ class JSONSourceMemo(object):
             raise Exception('Path (' + str(self.path) + ') does not exists.')
 
         # self.datafield_prefix = options.get('datafield-prefix', DATAFIELD)
-        self.tableRestricts = options['tables']
+        if 'tables' in options:
+            self.tableRestricts = options['tables'] 
+        else:
+            self.tableRestricts = None
+        self.file = options['file'] or "upgrade_memo.json"
 
 
     def __iter__(self):
         for item in self.previous:
             yield item
 
-        with open(os.path.join(self.path, "upgrade_memo.json"), "r") as read_file:
+        #import pdb; pdb.set_trace()    
+        with open(os.path.join(self.path, self.file), "r") as read_file:
           data = json.load(read_file)
 
-        for table in data:
-          if table['type'] == 'table' and table['name'] in self.tableRestricts:
-            print(table['name'])
-            for item in table['data']:
-              yield item
+        if self.tableRestricts:
+            for table in data:
+              if table['type'] == 'table' and table['name'] == self.tableRestricts:
+                print(table['name'])
+                for item in table['data']:
+                  yield item
+        else:
+            for item in data:
+                yield item          
+
+@provider(ISectionBlueprint)
+@implementer(ISection)
+class Mapping(object):
+    """
+    """
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+
+        # language mappings
+        languageDict = {
+            'Dutch': ['d0e5387'],
+            'Dutch (Ripuarian)': ['d0e5577'],      
+            'French': ['d0e3388'],      
+            'German': ['d0e9003'],      
+            'Greek': ['d0e7766'],      
+            'Italian': ['d0e7671'],      
+            'Latin': ['d0e8051'],      
+            'Portuguese': ['d0e7956'],
+            'Latin and German': ['d0e8051', 'd0e9003'],
+            'Latin and Italian': ['d0e8051', 'd0e7671']
+        }
+        self.storage = { 'language': languageDict}
+
+        self.tax_name = options['tax_name']
+        self.tax_field = options['tax_field']
+
+    def __iter__(self):
+        for item in self.previous:
+            #import pdb; pdb.set_trace()
+            if self.tax_field in item and self.tax_name in self.storage:
+                mapping = self.storage[self.tax_name]
+                key = item[self.tax_field]
+                if key in mapping:
+                   item[self.tax_field] =  mapping[key]
+
+            yield item
 
 
 @provider(ISectionBlueprint)
@@ -110,11 +147,18 @@ class InitStructureSource(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.initStructure = (
-                dict(_type='Folder', _path='/treatises',  title='Treatises', description='root'),
-                dict(_type='libraries', _path='/treatises/libraries', title='Libraries'),
-                dict(_type='authors', _path='/treatises/authors', title='Authors'),
-                dict(_type='manuscripts', _path='/treatises/manuscripts', title='Manuscripts'),
-                dict(_type='works', _path='/treatises/works', title='Works')
+                dict(_type='Folder', _path='/locations', title='Locations'),
+                dict(_type='Folder', _path='/persons', title='Persons'),
+                dict(_type='Folder', _path='/libraries', title='Libraries', description=''),
+                dict(_type='Folder', _path='/rules', title='Rules', description=''),                
+                dict(_type='Folder', _path='/treatises', title='Treatises', description=''),
+                dict(_type='Folder', _path='/treatises/manuscripts', title='Manuscripts'),
+                dict(_type='Folder', _path='/treatises/works', title='Works'),
+                dict(_type='Folder', _path='/treatises/manuscript_works', title='Manuscript Works'),
+                dict(_type='Folder', _path='/commentaries',title='Commentaries', description=''),
+                dict(_type='Folder', _path='/commentaries/manuscripts', title='Manuscripts'),
+                dict(_type='Folder', _path='/commentaries/works', title='Works'),
+                dict(_type='Folder', _path='/commentaries/manuscript_works', title='Manuscript Works')
                 )
 
     def __iter__(self):
@@ -140,6 +184,7 @@ class DictSource(object):
         for listitem in self.dictionary:
             yield listitem
 
+
 @provider(ISectionBlueprint)
 @implementer(ISection)
 class ReferenceUpdater(object):
@@ -160,62 +205,56 @@ class ReferenceUpdater(object):
             pathkeys = defaultKeys(options['blueprint'], name, 'path')
         self.pathkey = Matcher(*pathkeys)
 
+
+        self.source_type = options['source_type']
+        self.source_sqlIdField = options['source_sqlIdField']
         self.referenceFieldname = options['referenceFieldname']
-        self.sqlIdField = options['sqlIdField']
+        self.reference_type = options['reference_type']
+        self.reference_sqlIdField = options['reference_sqlIdField']
+        if 'sqlIdprefix' in options:
+            self.sqlIdprefix = options['sqlIdprefix']
+        else:
+           self.sqlIdprefix = ''
+        
+
 
         self.catalog = api.portal.get_tool('portal_catalog')
         self.intids = component.getUtility(IIntIds)
 
 
     def __iter__(self):
+        #import pdb; pdb.set_trace()
         for item in self.previous:
-            pathkey = self.pathkey(*item.keys())[0]
-            path = item[pathkey]
 
+            sourceId = str(item[self.source_sqlIdField])
+            referenceId = str(item[self.reference_sqlIdField])
+            
+            prefixed_sourceId = self.sqlIdprefix+sourceId
 
-            obj = self.context.unrestrictedTraverse(
-                safe_unicode(path).lstrip('/'), None)
-
-            if not obj:
+            srcObjBrain = self.catalog.unrestrictedSearchResults(sqlid=prefixed_sourceId, portal_type=self.source_type)[:1]
+            
+            if not srcObjBrain:
+                logger.warning('Reference: Source {0} of type {1} not found'.format(prefixed_sourceId, self.source_type))
                 yield item
                 continue
 
-            #import pdb; pdb.set_trace()
-
-            if self.sqlIdField not in item:
-                yield item
-                continue
-
-            if item[self.sqlIdField] is None:
-                yield item
-                continue   
-
-            try:    
-                linkedId = int(item[self.sqlIdField])
-            except ValueError as ex:
-                print('"%s" cannot be converted to an int: %s' % (item[self.sqlIdField], ex))
-                yield item
-                continue
-            except TypeError as ext:
-                print('"%s" cannot be converted to an int: %s' % (item[self.sqlIdField], ext))
-                yield item
-                continue       
-
-            relObjBrain = self.catalog.unrestrictedSearchResults(sqlid=linkedId)[:1]
+            srcObj = srcObjBrain[0].getObject()
             
 
-            if not relObjBrain:
+
+            prefixed_referenceId = self.sqlIdprefix+referenceId
+            referenceObjBrain = self.catalog.unrestrictedSearchResults(sqlid=prefixed_referenceId, portal_type=self.reference_type)[:1]
+            
+
+            if not referenceObjBrain:
+                logger.warning('Reference: Target {0} of type {1} not found, source is {2} of type {3}'.format(prefixed_referenceId, self.reference_type, srcObj.title, self.source_type))
                 yield item
                 continue
 
-            relObj = relObjBrain[0].getObject()
-            relObj_id = self.intids.getId(relObj)
+            referenceObj = referenceObjBrain[0].getObject()
 
-
-            if relObj_id >= 0:
-                setattr(obj, self.referenceFieldname, RelationValue(relObj_id))
-
-            notify(ObjectModifiedEvent(obj))
+            relapi.link_objects(
+                    srcObj, referenceObj, self.referenceFieldname)
 
             # always end with yielding the item,
             # unless you don't want it imported, or want
@@ -235,6 +274,7 @@ class SubtableLoader(object):
         self.options = options
         self.previous = previous
         self.context = transmogrifier.context
+        self.file = options['file'] or "upgrade_memo.json"
         self.tableRestricts = options['tables']
         self.linkinField = options['linkinField']
         self.linkinFieldSub = options['linkinFieldSub'] if 'linkinFieldSub' in options else options['linkinField']
@@ -246,24 +286,28 @@ class SubtableLoader(object):
 
 
     def __iter__(self):
+
         for item in self.previous:
             
+
             if self.linkinField not in item:
                 yield item
                 continue
 
-            with open(os.path.join(self.path, "upgrade_memo.json"), "r") as read_file:
+            with open(os.path.join(self.path, self.file), "r") as read_file:
                 data = json.load(read_file)
 
             
 
             for table in data:
-                if table['type'] == 'table' and table['name'] in self.tableRestricts:
+                if table['type'] == 'table' and table['name'] == self.tableRestricts:
+
                     l = []
                     for subitem in table['data']:
                         if self.linkinFieldSub in subitem:
-                            if subitem[self.linkinFieldSub] == item[self.linkinField]:
-                               l.append(subitem[self.valueField])
+                            if str(subitem[self.linkinFieldSub]) == str(item[self.linkinField]):
+                                #import pdb; pdb.set_trace()
+                                l.append(subitem[self.valueField])
                     if l:
                         item[self.targetField] = l
 
@@ -272,6 +316,69 @@ class SubtableLoader(object):
                   
             yield item            
 
+
+@provider(ISectionBlueprint)
+@implementer(ISection)
+class SubtableLinkLoader(object):
+    """
+    """
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.file = options['file'] or "upgrade_memo.json"
+        self.tableRestricts = options['tables']
+        self.linkinField = options['linkinField']
+        self.linkinFieldSub = options['linkinFieldSub'] if 'linkinFieldSub' in options else options['linkinField']
+        self.targetField = options['targetField']
+        self.path = resolvePackageReferenceOrFile(options['path'])
+        if self.path is None or not os.path.isdir(self.path):
+            raise Exception('Path (' + str(self.path) + ') does not exists.')
+
+
+    def __iter__(self):
+
+        for item in self.previous:
+            
+
+            if self.linkinField not in item:
+                yield item
+                continue
+
+            with open(os.path.join(self.path, self.file), "r") as read_file:
+                data = json.load(read_file)
+
+            
+
+            for table in data:
+                if table['type'] == 'table' and table['name'] == self.tableRestricts:
+
+                    l = []
+                    for subitem in table['data']:
+                        if self.linkinFieldSub in subitem:
+                            if str(subitem[self.linkinFieldSub]) == str(item[self.linkinField]):
+                                #import pdb; pdb.set_trace()
+                                if subitem['link_url'] and subitem['link_url']!='':
+                                    
+                                    field = URI()
+                                    try:
+                                        field.fromUnicode(subitem['link_url'])
+                                        entry = dict(_class='saw.memo.contenttypes.behaviour.literature.Link', text=subitem['link_title'], url=subitem['link_url'])
+                                        l.append(entry)
+                                    except:
+                                        url = subitem['link_url']
+                                        print('{0} is not a valid url'.format(url))
+                                            
+                    if l:
+                        item[self.targetField] = l
+
+
+                     
+                  
+            yield item      
 
 @implementer(ISection)
 @provider(ISectionBlueprint)
@@ -348,3 +455,52 @@ class GrantRoles(object):
                     transaction.commit()
            
             yield item            
+
+
+@provider(ISectionBlueprint)
+@implementer(ISection)
+class DatesUpdater(object):
+    """Sets creation and modification dates on objects.
+    """
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
+        self.creationkey = options.get('creation-key', 'creation_date')
+        self.modificationkey = options.get('modification-key', 'modification_date')  # noqa
+        self.effectivekey = options.get('effective-key', 'effective_date')
+        self.expirationkey = options.get('expiration-key', 'expiration_date')
+
+    def __iter__(self):
+
+        #import pdb; pdb.set_trace()
+        for item in self.previous:
+            pathkey = self.pathkey(*list(item.keys()))[0]
+            if not pathkey:  # not enough info
+                yield item
+                continue
+            path = item[pathkey]
+
+            ob = traverse(self.context, str(path).lstrip('/'), None)
+            if ob is None:
+                yield item
+                continue  # object not found
+
+            creationdate = item.get(self.creationkey, None)
+            if creationdate and hasattr(ob, 'creation_date'):
+                ob.creation_date = datetime.fromtimestamp(int(creationdate))
+
+            modificationdate = item.get(self.modificationkey, None)
+            if modificationdate and hasattr(ob, 'modification_date'):
+                ob.modification_date = datetime.fromtimestamp(int(modificationdate))
+
+            effectivedate = item.get(self.effectivekey, None)
+            if effectivedate and hasattr(ob, 'effective_date'):
+                ob.effective_date = datetime.fromtimestamp(int(effectivedate))
+
+            expirationdate = item.get(self.expirationkey, None)
+            if expirationdate and hasattr(ob, 'expiration_date'):
+                ob.expiration_date = datetime.fromtimestamp(int(expirationdate))
+
+            yield item
