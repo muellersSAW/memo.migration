bb64ba340f4b5dbe0f87a93e4776baa227794458
muellers-saw-leipzig <muellers@saw-leipzig.de>
2021-08-12 14:46:27 +0200

    override DefaultSerializer to be abe to strip Spaces from input strings

--- src/memo/migration/factories.py
+++ src/memo/migration/factories.py
@@ -14,8 +14,9 @@ from zope.component import queryUtility
 from zope.dottedname.resolve import resolve
 from zope.interface import implementer
 from zope.schema.interfaces import IDate
-
-
+from zope.schema.interfaces import IField
+from Products.CMFPlone.utils import safe_unicode
+from zope.schema.interfaces import IFromUnicode
 
 @implementer(IDeserializer)
 @adapter(IDate)
@@ -49,4 +50,35 @@ class DateDeserializer(object):
                             item['_path'],
                             e)
                     )
+        return value
+
+
+@implementer(IDeserializer)
+@adapter(IField)
+class DefaultDeserializer(object):
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value, filestore, item,
+                 disable_constraints=False, logger=None):
+        field = self.field
+        if field is not None:
+            try:
+                if isinstance(value, str):
+                    value = safe_unicode(value.strip())
+                if str(type(value)) == "<type 'unicode'>":
+                    value = IFromUnicode(field).fromUnicode(value.strip())
+                self.field.validate(value)
+            except Exception as e:
+                if not disable_constraints:
+                    raise e
+                else:
+                    if logger:
+                        logger(
+                            "DefaultDeserializer: %s is invalid in %s: %s" % (
+                                self.field.__name__,
+                                item['_path'],
+                                e.__repr__())
+                        )
         return value
